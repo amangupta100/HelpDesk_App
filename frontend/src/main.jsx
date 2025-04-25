ae01aa408c705c06b5c6b628f85c6b5692254ad4
Aman Gupta <coderamang02@gmail.com>
2025-04-25 14:05:08 +0530

    browsR. to hashR. change in main

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,12 +1,12 @@
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import {App} from './App.jsx'
-import {BrowserRouter} from 'react-router-dom'
+import {HashRouter} from 'react-router-dom'
 import {Toaster} from 'react-hot-toast'
 
 createRoot(document.getElementById('root')).render(
-    <BrowserRouter>
+    <HashRouter>
     <Toaster/>
     <App />
-    </BrowserRouter>
+    </HashRouter>
 )
